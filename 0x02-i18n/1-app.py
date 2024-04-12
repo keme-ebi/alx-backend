d8ac283adfb585a3de8c49767ad46568d1c7fe57
keme-ebi <kelvin6bolou@gmail.com>
2024-04-12 10:10:23 +0100

    flask_babel basic config

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Flask babel"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """render html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
