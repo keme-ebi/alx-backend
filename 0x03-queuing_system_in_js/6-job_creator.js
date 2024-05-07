dad55d6fd976274e5f04cfddf5b549d2635141bb
keme-ebi <kelvin6bolou@gmail.com>
2024-05-07 23:40:25 +0100

    job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '11223344',
-  message: 'Working on queue with Kue',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData).save((err) => {
